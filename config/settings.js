c766960dddf053f739b8feea76c7cd71dee27e6d
xc54677 <gesangcx@outlook.com>
2026-02-06 19:29:02 +0800

    补全收集信息页面代码

--- config/settings.js
+++ config/settings.js
@@ -1,8 +1,10 @@
-const rootUrl='http://192.168.1.3:8000/smartcommunity'
+// const rootUrl='http://192.168.2.8:8000/smartcommunity'
+const rootUrl='https://5c3f28c8.r26.cpolar.top/smartcommunity'
 
 //表示导出--》在任意js中可以导入--》导入的就是下面的对象
 module.exports={
   welcome: rootUrl + '/welcome/',
   banner: rootUrl + '/banner/',
   collection: rootUrl + '/collection/',
+  area: rootUrl + '/area/',
 }
--- pages/form/form.js
+++ pages/form/form.js
@@ -1,11 +1,67 @@
-// pages/form/form.js
+import api from '../../config/settings'
+
 Page({
   data: {
     avatar: '/images/camera/camera.png',
+    objectArray: [{
+        id: 1,
+        name: '2单元1号楼',
+        desc: '2#1'
+      },
+      {
+        id: 2,
+        name: '2单元2号楼',
+        desc: '2#2'
+      }
+    ],
+    name: '',
+    index: -1,
   },
-  bindToCamera(){
+  bindToCamera() {
     wx.navigateTo({
       url: '/pages/camera/camera'
+    })
+  },
+  onLoad() {
+    wx.request({
+      url: api.area,
+      method: 'GET',
+      success: (res) => {
+        this.setData({
+          objectArray: res.data
+        })
+      }
+    })
+  },
+  //选中某个网格回现
+  bindPickerChange(e) {
+    this.setData({
+      index: e.detail.value
+    })
+  },
+
+  //上传图片
+  postUser() {
+    wx.showLoading({
+      title: '提交中',
+      mask: true
+    })
+
+    wx.uploadFile({
+      url: api.collection,
+      filePath: this.data.avatar,
+      name: 'avatar',
+      formData: {
+        name: this.data.name,
+        area: this.data.objectArray[this.data.index].id //选中了哪个区域的id
+      },
+      success: (res) => {
+        //跳转回上一页
+        wx.navigateBack({})
+      },
+      complete: () => {
+        wx.hideLoading()
+      }
     })
   }
 })
